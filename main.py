e9a965b602530b9ee9308e548203d3c851bc9e50
Maxim Teselkin <91027877+MaxTeselkin@users.noreply.github.com>
2023-01-15 04:19:09 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# import necessary libraries
 import supervisely as sly
 from supervisely.geometry.graph import Node, KeypointsTemplate
 import os
@@ -6,42 +7,28 @@ import matplotlib.image as mpimg
 import json
 from dotenv import load_dotenv
 
-# authentication
-# # learn more here - https://developer.supervise.ly/getting-started/first-steps/basics-of-authentication
-load_dotenv(os.path.expanduser("~/supervisely.env"))
-api = sly.Api.from_env()
-my_teams = api.team.get_list()
-team = my_teams[0]
-workspace = api.workspace.get_list(team.id)[0]
 
-# create project and dataset
-project = api.project.create(
-    workspace.id, "Surfer Pose Estimation", change_name_if_conflict=True
-)
-dataset = api.dataset.create(project.id, "Surfing", change_name_if_conflict=True)
-print(f"Project {project.id} with dataset {dataset.id} are created")
-
-# build geometry config
-# initialize template
+# working with keypoints template
+# create keypoints template
 template = KeypointsTemplate()
 # add nodes
-template.add_point(label="nose", row=515, col=248, color=[255, 0, 0])
-template.add_point(label="left_eye", row=536, col=230, color=[255, 0, 0])
-template.add_point(label="right_eye", row=496, col=228, color=[255, 0, 0])
-template.add_point(label="left_ear", row=561, col=248, color=[255, 0, 0])
-template.add_point(label="right_ear", row=464, col=242, color=[255, 0, 0])
-template.add_point(label="left_shoulder", row=615, col=367, color=[255, 0, 0])
-template.add_point(label="right_shoulder", row=400, col=383, color=[255, 0, 0])
-template.add_point(label="left_elbow", row=653, col=559, color=[255, 0, 0])
-template.add_point(label="right_elbow", row=364, col=554, color=[255, 0, 0])
-template.add_point(label="left_wrist", row=655, col=712, color=[255, 0, 0])
-template.add_point(label="right_wrist", row=364, col=711, color=[255, 0, 0])
-template.add_point(label="left_hip", row=565, col=703, color=[255, 0, 0])
-template.add_point(label="right_hip", row=437, col=705, color=[255, 0, 0])
-template.add_point(label="left_knee", row=568, col=922, color=[255, 0, 0])
-template.add_point(label="right_knee", row=434, col=919, color=[255, 0, 0])
-template.add_point(label="left_ankle", row=571, col=1109, color=[255, 0, 0])
-template.add_point(label="right_ankle", row=442, col=1100, color=[255, 0, 0])
+template.add_point(label="nose", row=635, col=427)
+template.add_point(label="left_eye", row=597, col=404)
+template.add_point(label="right_eye", row=685, col=401)
+template.add_point(label="left_ear", row=575, col=431)
+template.add_point(label="right_ear", row=723, col=425)
+template.add_point(label="left_shoulder", row=502, col=614)
+template.add_point(label="right_shoulder", row=794, col=621)
+template.add_point(label="left_elbow", row=456, col=867)
+template.add_point(label="right_elbow", row=837, col=874)
+template.add_point(label="left_wrist", row=446, col=1066)
+template.add_point(label="right_wrist", row=845, col=1073)
+template.add_point(label="left_hip", row=557, col=1035)
+template.add_point(label="right_hip", row=743, col=1043)
+template.add_point(label="left_knee", row=541, col=1406)
+template.add_point(label="right_knee", row=751, col=1421)
+template.add_point(label="left_ankle", row=501, col=1760)
+template.add_point(label="right_ankle", row=774, col=1765)
 # add edges
 template.add_edge(src="left_ankle", dst="left_knee", color=[255, 0, 0])
 template.add_edge(src="left_knee", dst="left_hip", color=[255, 0, 0])
@@ -62,75 +49,70 @@ template.add_edge(src="left_eye", dst="left_ear", color=[255, 0, 0])
 template.add_edge(src="right_eye", dst="right_ear", color=[255, 0, 0])
 template.add_edge(src="left_ear", dst="left_shoulder", color=[255, 0, 0])
 template.add_edge(src="right_ear", dst="right_shoulder", color=[255, 0, 0])
+# visualize your keypoints template
+template_img = sly.image.read("images/girl.jpg")
+template.draw(image=template_img)
+sly.image.write("images/template.jpg", template_img)
+# you can also transfer your template to json
+template_json = template.to_json()
 
+
+# programmatically create keypoints annotation
+# authentication
+# learn more here - https://developer.supervise.ly/getting-started/first-steps/basics-of-authentication
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+api = sly.Api.from_env()
+my_teams = api.team.get_list()
+team = my_teams[0]
+workspace = api.workspace.get_list(team.id)[0]
+# create project and dataset
+project = api.project.create(workspace.id, "Human Pose Estimation", change_name_if_conflict=True)
+dataset = api.dataset.create(project.id, "Person with dog", change_name_if_conflict=True)
+print(f"Project {project.id} with dataset {dataset.id} are created")
 # create annotation class
-surfer = sly.ObjClass("surfer", geometry_type=sly.GraphNodes, geometry_config=template)
-project_meta = sly.ProjectMeta(obj_classes=[surfer])
+person = sly.ObjClass("person", geometry_type=sly.GraphNodes, geometry_config=template)
+project_meta = sly.ProjectMeta(obj_classes=[person])
 api.project.update_meta(project.id, project_meta.to_json())
-
-# upload image to the dataset
+# upload image
 image_info = api.image.upload_path(
-    dataset.id, name="Surfer.jpg", path="images/Surfer.jpg"
+    dataset.id, name="person_with_dog.jpg", path="images/person_with_dog.jpg"
 )
-
 # create annotation and upload to image
 # build keypoints graph
-v1 = sly.Node(label="nose", row=364, col=775)
-v2 = sly.Node(label="left_eye", row=353, col=773)
-v3 = sly.Node(label="right_eye", row=352, col=766)
-v4 = sly.Node(label="left_ear", row=358, col=736)
-v5 = sly.Node(label="right_ear", row=357, col=752)
-v6 = sly.Node(label="left_shoulder", row=447, col=747)
-v7 = sly.Node(label="right_shoulder", row=403, col=649)
-v8 = sly.Node(label="left_elbow", row=531, col=767)
-v9 = sly.Node(label="right_elbow", row=448, col=545)
-v10 = sly.Node(label="left_wrist", row=585, col=782)
-v11 = sly.Node(label="right_wrist", row=538, col=507)
-v12 = sly.Node(label="left_hip", row=567, col=624)
-v13 = sly.Node(label="right_hip", row=545, col=576)
-v14 = sly.Node(label="left_knee", row=700, col=668)
-v15 = sly.Node(label="right_knee", row=646, col=704)
-v16 = sly.Node(label="left_ankle", row=769, col=547)
-v17 = sly.Node(label="right_ankle", row=802, col=665)
-
 nodes = [
-    v1,
-    v2,
-    v3,
-    v4,
-    v5,
-    v6,
-    v7,
-    v8,
-    v9,
-    v10,
-    v11,
-    v12,
-    v13,
-    v14,
-    v15,
-    v16,
-    v17,
+    sly.Node(label="nose", row=146, col=702),
+    sly.Node(label="left_eye", row=130, col=644),
+    sly.Node(label="right_eye", row=135, col=701),
+    sly.Node(label="left_ear", row=137, col=642),
+    sly.Node(label="right_ear", row=142, col=705),
+    sly.Node(label="left_shoulder", row=221, col=595),
+    sly.Node(label="right_shoulder", row=226, col=738),
+    sly.Node(label="left_elbow", row=335, col=564),
+    sly.Node(label="right_elbow", row=342, col=765),
+    sly.Node(label="left_wrist", row=429, col=555),
+    sly.Node(label="right_wrist", row=438, col=784),
+    sly.Node(label="left_hip", row=448, col=620),
+    sly.Node(label="right_hip", row=451, col=713),
+    sly.Node(label="left_knee", row=598, col=591),
+    sly.Node(label="right_knee", row=602, col=715),
+    sly.Node(label="left_ankle", row=761, col=573),
+    sly.Node(label="right_ankle", row=766, col=709),
 ]
-
-
 # label the image
-label = sly.Label(sly.GraphNodes(nodes), surfer)
-ann = sly.Annotation(img_size=[1280, 1920], labels=[label])
+input_image = sly.image.read("images/person_with_dog.jpg")
+img_height, img_width = input_image.shape[:2]
+label = sly.Label(sly.GraphNodes(nodes), person)
+ann = sly.Annotation(img_size=[img_height, img_width], labels=[label])
 api.annotation.upload_ann(image_info.id, ann)
-
-# download image and annotation
+# download data
 image = api.image.download_np(image_info.id)
 print("image shape (height, width, channels)", image.shape)
-
 ann_json = api.annotation.download_json(image_info.id)
 print("annotaiton:\n", json.dumps(ann_json, indent=4))
-
 # draw annotation
 ann = sly.Annotation.from_json(ann_json, project_meta)
-output_path = "images/Labeled.jpg"
+output_path = "images/person_with_dog_labelled.jpg"
 ann.draw_pretty(image, output_path=output_path, thickness=3)
-
 # function for image visualization
 def visualize_image(image_filepath):
     plt.figure(figsize=(12, 8))
